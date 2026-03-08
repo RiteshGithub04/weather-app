d9ce76af351e0684016e78a9a23d00df7743d68f
Ritesh Jagtap <jagtapritesh16@gmail.com>
2026-03-08 07:54:09 +0000

    Initial Commit

---
+++ weather-app/script.js
@@ -0,0 +1,16 @@
+function getWeather() {
+    const city = document.getElementById("city").value;
+
+    if(city === "") {
+        alert("Please enter a city name");
+        return;
+    }
+
+    const weatherResult = document.getElementById("weather-result");
+
+    weatherResult.innerHTML = `
+        <p>Weather in <strong>${city}</strong></p>
+        <p>Temperature: 25°C</p>
+        <p>Condition: Sunny ☀️</p>
+    `;
+}
